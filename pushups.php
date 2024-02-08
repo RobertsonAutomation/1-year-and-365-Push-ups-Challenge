22bcd23cc020a3d459ea46ba923ce7fa7240d21f
Ian Robertson <18728598+RobertsonAutomation@users.noreply.github.com>
2024-02-07 22:12:53 -0500

    Update pushups.php

--- pushups.php
+++ pushups.php
@@ -1,7 +1,7 @@
 <?php
 $pushups = 0;
 $total = 0;
-$timestamp = strtotime("December 31, 2018");
+$timestamp = strtotime("December 31, 2023");
 
 while($pushups < 365) {
   $timestamp = strtotime("+ 1 days", $timestamp);
